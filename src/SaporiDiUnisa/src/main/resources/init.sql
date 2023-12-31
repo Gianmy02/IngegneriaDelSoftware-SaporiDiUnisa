06133c354a8821f96ad8eb8fcde28af675313557
Salvatore Ruocco <114869138+tatore02@users.noreply.github.com>
2023-12-31 18:55:32 +0100

    Update init.sql

--- src/SaporiDiUnisa/src/main/resources/init.sql
+++ src/SaporiDiUnisa/src/main/resources/init.sql
@@ -2,19 +2,19 @@ create database if not exists saporiDiUnisa;
 use saporiDiUnisa;
 
 create table if not exists dipendente(
-                                         id int not null auto_increment primary key,
-                                         ruolo enum('admin', 'cassiere', 'magazziniere', 'finanze') not null,
+    id int not null auto_increment primary key,
+    ruolo enum('admin', 'cassiere', 'magazziniere', 'finanze') not null,
     pin char(4) not null
-    );
+);
 
 create table if not exists fornitura(
-                                        id int not null auto_increment primary key,
-                                        giorno date not null
+    id int not null auto_increment primary key,
+    giorno date not null
 );
 
 create table if not exists prodotto(
-                                       id int not null auto_increment primary key,
-                                       nome varchar(255) not null,
+    id int not null auto_increment primary key,
+    nome varchar(255) not null,
     marchio varchar(255) not null,
     prezzo decimal(5,2) not null,
     prezzo_scontato decimal(5,2),
@@ -23,13 +23,23 @@ create table if not exists prodotto(
     foto mediumblob not null,
     check(prezzo >= 0),
     check(prezzo_scontato > 0 and prezzo_scontato < prezzo),
-    check(inizio_sconto > current_date),
+    # check(inizio_sconto > current_date),
     check(inizio_sconto <= fine_sconto)
-    );
+);
+
+create trigger if not exists check_inizio_sconto
+    before insert on prodotto
+    for each row
+begin
+    if NEW.inizio_sconto <= CURRENT_DATE THEN
+        SIGNAL SQLSTATE '45000'
+            SET MESSAGE_TEXT = 'La data di inizio_sconto deve essere maggiore di quella odierna';
+    end if;
+end;
 
 create table if not exists lotto(
-                                    id int not null auto_increment primary key,
-                                    costo decimal(5,2) not null,
+    id int not null auto_increment primary key,
+    costo decimal(5,2) not null,
     data_scadenza date not null,
     quantita int not null,
     quantita_attuale int not null,
@@ -38,14 +48,14 @@ create table if not exists lotto(
     check(costo > 0),
     check(quantita > 0),
     check(quantita_attuale >= 0)
-    );
+);
 
 create table if not exists vendita(
-                                      giorno date not null primary key
+    giorno date not null primary key
 );
 
 create table if not exists vendita_prodotto(
-                                               costo decimal(5,2) not null,
+    costo decimal(5,2) not null,
     quantita int not null,
     guadagno decimal(5,2) not null,
     prodotto int not null references prodotto(id) on delete cascade,
@@ -54,12 +64,12 @@ create table if not exists vendita_prodotto(
     check(costo > 0),
     check(quantita > 0),
     check(guadagno > 0)
-    );
+);
 
 create table if not exists esposizione(
-                                          prodotto int not null references prodotto(id) on delete cascade,
+    prodotto int not null references prodotto(id) on delete cascade,
     lotto int not null references lotto(id) on delete cascade,
     quantita int not null,
     primary key(lotto, prodotto),
     check(quantita > 0)
-    );
+);
