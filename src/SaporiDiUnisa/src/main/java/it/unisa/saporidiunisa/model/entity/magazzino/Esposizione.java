f4df3c186fc6c1da7e17a740c535c6f31f7582f3
afacchiano <114217855+afacchiano@users.noreply.github.com>
2024-01-06 17:18:33 +0100

    Fix entities

--- src/SaporiDiUnisa/src/main/java/it/unisa/saporidiunisa/model/entity/magazzino/Esposizione.java
+++ src/SaporiDiUnisa/src/main/java/it/unisa/saporidiunisa/model/entity/magazzino/Esposizione.java
@@ -1,15 +1,13 @@
 package it.unisa.saporidiunisa.model.entity.magazzino;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
 public class Esposizione
 {
-    private final Prodotto prodotto;
-    private final Lotto lotto;
+    private Prodotto prodotto;
+    private Lotto lotto;
     private int quantita;
 }
