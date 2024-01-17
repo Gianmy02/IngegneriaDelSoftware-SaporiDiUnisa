73f74e9d86eea49596281be17a2cabc72cf2bd96
simonevittoria <s.vittoria@studenti.unisa.it>
2024-01-17 18:05:16 +0100

    ho accentuato il rosso

--- src/SaporiDiUnisa/src/main/java/it/unisa/saporidiunisa/controller/scaffale/servlet/VisualizzaScaffale.java
+++ src/SaporiDiUnisa/src/main/java/it/unisa/saporidiunisa/controller/scaffale/servlet/VisualizzaScaffale.java
@@ -1,14 +1,18 @@
 package it.unisa.saporidiunisa.controller.scaffale.servlet;
 
 import it.unisa.saporidiunisa.controller.scaffale.ScaffaleController;
+import it.unisa.saporidiunisa.model.entity.Dipendente;
 import it.unisa.saporidiunisa.model.entity.Esposizione;
 import it.unisa.saporidiunisa.model.entity.Lotto;
+import it.unisa.saporidiunisa.utils.Errors;
+import it.unisa.saporidiunisa.utils.Utils;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.val;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -18,7 +22,22 @@ public class VisualizzaScaffale extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        val session = req.getSession();
+
+        val dipendente = (Dipendente)session.getAttribute("dipendente");
+        if (dipendente == null || dipendente.getRuolo() != Dipendente.Ruolo.MAGAZZINIERE)
+        {
+            Utils.dispatchError(Errors.NO_PERMISSIONS, req, resp);
+            return;
+        }
+
         String address = req.getParameter("address");
+        if (address == null)
+        {
+            Utils.dispatchError(Errors.INVALID_FIELD.formatted("indirizzo"), req, resp);
+            return;
+        }
+
         ArrayList<Esposizione> lottiScaffale = ScaffaleController.visualizzaProdottiScaffale();
         req.setAttribute("lottiScaffale", lottiScaffale);
 
@@ -26,13 +45,17 @@ public class VisualizzaScaffale extends HttpServlet {
             ArrayList<Lotto> lottiMagazzino = ScaffaleController.visualizzaProdottiMagazzino();
             req.setAttribute("lottiMagazzino", lottiMagazzino);
         }
-        else
+        else if(address.equalsIgnoreCase("visualizza_scaffale"))
         {
             ArrayList<Esposizione> lottiScaffaleScaduti = ScaffaleController.visualizzaProdottiScaffaleScaduti();
             req.setAttribute("lottiScaffaleScaduti", lottiScaffaleScaduti);
         }
+        else
+        {
+            Utils.dispatchError(Errors.INVALID_FIELD.formatted("indirizzo"), req, resp);
+            return;
+        }
 
-        RequestDispatcher dispatcher = req.getRequestDispatcher("view/" + address + ".jsp");
-        dispatcher.forward(req, resp);
+        req.getRequestDispatcher("view/" + address + ".jsp").forward(req, resp);
     }
 }
--- src/SaporiDiUnisa/src/main/webapp/view/aggiunta_scaffale.jsp
+++ src/SaporiDiUnisa/src/main/webapp/view/aggiunta_scaffale.jsp
@@ -8,11 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Sapori di Unisa - Aggiunta Scaffale</title>
         <link rel="stylesheet" href="${pageContext.request.contextPath}/style/style.css">
-        <link rel="stylesheet" href="${pageContext.request.contextPath}/style/table.css">
-        <link rel="apple-touch-icon" sizes="180x180" href="${pageContext.request.contextPath}/img/favicon/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="${pageContext.request.contextPath}/img/favicon/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="${pageContext.request.contextPath}/img/favicon/favicon-16x16.png">
-        <link rel="manifest" href="${pageContext.request.contextPath}/img/favicon/site.webmanifest">
+        <link rel="stylesheet" href="${pageContext.request.contextPath}/style/scaffale.css">
         <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
         <script src="${pageContext.request.contextPath}/script/aggiuntaScaffale.js" defer></script>
     </head>
@@ -94,9 +90,11 @@
             </table>
         </div>
 
-            <input id="confirm-button" type="submit" value="CONFERMA">
+            <input class="btn" id="confirm-button" type="submit" value="CONFERMA">
 
         </form>
+
+        <a href="${pageContext.request.contextPath}/view/select_VisualizzaScaffale_AggiungiProdottiScaffale.jsp"><button class="btn" id="indietro-button">INDIETRO</button></a>
 
     </main>
     <%@ include file="/WEB-INF/include/footer.html" %>
