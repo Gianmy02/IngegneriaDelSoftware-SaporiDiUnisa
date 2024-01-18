41d26b2cfee62296cb2d097a420e51c82fda66ef
Salvatore Ruocco <114869138+tatore02@users.noreply.github.com>
2024-01-18 01:46:29 +0100

    Fix login

--- src/SaporiDiUnisa/src/main/java/it/unisa/saporidiunisa/controller/autenticazione/servlet/LoginServlet.java
+++ src/SaporiDiUnisa/src/main/java/it/unisa/saporidiunisa/controller/autenticazione/servlet/LoginServlet.java
@@ -21,41 +21,38 @@ public class LoginServlet extends HttpServlet
     {
         val session = request.getSession();
 
-        if (session.getAttribute("dipendente") == null)
+        val pin = request.getParameter("pin");
+        if (pin == null)
         {
-            val pin = request.getParameter("pin");
-            if (pin == null)
-            {
-                Utils.dispatchError(Errors.INVALID_FIELD.formatted("pin"), request, response);
-                return;
-            }
-
-            val matcherPin = Patterns.LOGIN_PIN.matcher(pin);
-            if (!matcherPin.matches())
-            {
-                Utils.dispatchError(Errors.INVALID_FORMAT.formatted("pin"), request, response);
-                return;
-            }
-
-            val dipendente = AutenticazioneController.login(pin);
-            if (dipendente == null)
-            {
-                Utils.dispatchError(Errors.NOT_FOUND.formatted("dipendente"), request, response);
-                return;
-            }
-
-            session.setAttribute("dipendente", dipendente);
-
-            val address = switch (dipendente.getRuolo())
-            {
-                case CASSIERE -> "view/select/select_Cassiere.jsp";
-                case ADMIN -> "view/select/select_admin.jsp";
-                case FINANZE -> "view/select/finanze.jsp";
-                case MAGAZZINIERE -> "view/select/select_Magazzino_Scaffale.jsp";
-            };
-
-            response.sendRedirect(address);
+            Utils.dispatchError(Errors.INVALID_FIELD.formatted("pin"), request, response);
+            return;
         }
+
+        val matcherPin = Patterns.LOGIN_PIN.matcher(pin);
+        if (!matcherPin.matches())
+        {
+            Utils.dispatchError(Errors.INVALID_FORMAT.formatted("pin"), request, response);
+            return;
+        }
+
+        val dipendente = AutenticazioneController.login(pin);
+        if (dipendente == null)
+        {
+            Utils.dispatchError(Errors.NOT_FOUND.formatted("dipendente"), request, response);
+            return;
+        }
+
+        session.setAttribute("dipendente", dipendente);
+
+        val address = switch (dipendente.getRuolo())
+        {
+            case CASSIERE -> "view/select/select_Cassiere.jsp";
+            case ADMIN -> "view/select/select_admin.jsp";
+            case FINANZE -> "view/select/finanze.jsp";
+            case MAGAZZINIERE -> "view/select/select_Magazzino_Scaffale.jsp";
+        };
+
+        response.sendRedirect(address);
     }
 }
 
