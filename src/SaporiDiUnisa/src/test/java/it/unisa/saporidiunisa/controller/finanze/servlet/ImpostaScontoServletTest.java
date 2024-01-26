ceb4f419302058bec1cc7b3a77bace1c492aa1df
afacchiano <114217855+afacchiano@users.noreply.github.com>
2024-01-26 21:52:55 +0100

    Fix ImpostaScontoServletTest

--- src/SaporiDiUnisa/src/test/java/it/unisa/saporidiunisa/controller/finanze/servlet/ImpostaScontoServletTest.java
+++ src/SaporiDiUnisa/src/test/java/it/unisa/saporidiunisa/controller/finanze/servlet/ImpostaScontoServletTest.java
@@ -5,11 +5,7 @@ import it.unisa.saporidiunisa.controller.magazzino.MagazzinoController;
 import it.unisa.saporidiunisa.model.entity.Dipendente;
 import it.unisa.saporidiunisa.model.entity.Prodotto;
 import it.unisa.saporidiunisa.utils.Utils;
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.val;
 import org.junit.jupiter.api.*;
 import org.mockito.Answers;
@@ -35,14 +31,16 @@ class ImpostaScontoServletTest extends ServletTest
     }
 
     @Nested
-    class Incorrect {
+    class Incorrect
+    {
         @AfterEach
-        void afterEach() throws ServletException, IOException {
+        void afterEach() throws ServletException, IOException
+        {
             try (val utils = mockStatic(Utils.class, Answers.CALLS_REAL_METHODS);
                  val magazzinoController = mockStatic(MagazzinoController.class, Answers.CALLS_REAL_METHODS))
             {
                 val prodotto1 = new Prodotto(1, "Farina", "Caputo", 10.00F, 0, null, null, null);
-                val prodotto2 = new Prodotto(5, "Farina", "Caputo", 10.00F, 7.50F, LocalDate.of(2024,3,3), LocalDate.of(2024,3,5), null);
+                val prodotto2 = new Prodotto(5, "Farina", "Caputo", 10.00F, 7.50F, LocalDate.of(2024, 3, 3), LocalDate.of(2024, 3, 5), null);
 
                 magazzinoController.when(() -> MagazzinoController.getProdottoById(1)).thenReturn(prodotto1);
                 magazzinoController.when(() -> MagazzinoController.getProdottoById(5)).thenReturn(prodotto2);
@@ -56,79 +54,99 @@ class ImpostaScontoServletTest extends ServletTest
             }
         }
 
-
         @Test
         @DisplayName("5.2.1")
-        void tc_5_2_1() {
-            populateRequest(ofEntries(entry("prodotto", "0"), entry("dataInizioSconto","2025-01-01"), entry("dataFineSconto","2025-01-02"), entry("sconto","1")));
+        void tc_5_2_1()
+        {
+            populateRequest(ofEntries(entry("prodotto", "0"), entry("dataInizioSconto", "2025-01-01"), entry("dataFineSconto", "2025-01-02"), entry("sconto", "1")));
         }
 
         @Test
         @DisplayName("5.2.2")
-        void tc_5_2_2() {
-            populateRequest(ofEntries(entry("prodotto", "5"), entry("dataInizioSconto","2025-01-01"), entry("dataFineSconto","2025-01-02"), entry("sconto","1")));
+        void tc_5_2_2()
+        {
+            populateRequest(ofEntries(entry("prodotto", "5"), entry("dataInizioSconto", "2025-01-01"), entry("dataFineSconto", "2025-01-02"), entry("sconto", "1")));
         }
 
         @Test
         @DisplayName("5.2.3")
-        void tc_5_2_3() {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2025-01-00"), entry("dataFineSconto","2025-01-02"), entry("sconto","1")));
+        void tc_5_2_3()
+        {
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2025-01-00"), entry("dataFineSconto", "2025-01-02"), entry("sconto", "1")));
         }
 
         @Test
         @DisplayName("5.2.4")
-        void tc_5_2_4() {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2020-01-01"), entry("dataFineSconto","2025-01-02"), entry("sconto","1")));
+        void tc_5_2_4()
+        {
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2020-01-01"), entry("dataFineSconto", "2025-01-02"), entry("sconto", "1")));
         }
 
         @Test
         @DisplayName("5.2.5")
-        void tc_5_2_5() {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2025-01-01"), entry("dataFineSconto","2025-01-00"), entry("sconto","1")));
+        void tc_5_2_5()
+        {
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2025-01-01"), entry("dataFineSconto", "2025-01-00"), entry("sconto", "1")));
         }
 
         @Test
         @DisplayName("5.2.6")
-        void tc_5_2_6() {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2025-01-02"), entry("dataFineSconto","2025-01-01"), entry("sconto","1")));
+        void tc_5_2_6()
+        {
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2025-01-02"), entry("dataFineSconto", "2025-01-01"), entry("sconto", "1")));
         }
 
         @Test
         @DisplayName("5.2.7")
-        void tc_5_2_7() {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2025-01-01"), entry("dataFineSconto","2025-01-02"), entry("sconto","-1")));
+        void tc_5_2_7()
+        {
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2025-01-01"), entry("dataFineSconto", "2025-01-02"), entry("sconto", "-1")));
         }
 
         @Test
         @DisplayName("5.2.8")
-        void tc_5_2_8() {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2025-01-01"), entry("dataFineSconto","2025-01-00"), entry("sconto","101")));
+        void tc_5_2_8()
+        {
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2025-01-01"), entry("dataFineSconto", "2025-01-00"), entry("sconto", "101")));
         }
 
         @Test
         @DisplayName("5.2.9")
-        void tc_5_2_9() {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2025-01-01"), entry("dataFineSconto","2025-01-00"), entry("sconto","50a")));
+        void tc_5_2_9()
+        {
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2025-01-01"), entry("dataFineSconto", "2025-01-00"), entry("sconto", "50a")));
         }
     }
+
     @Nested
     class Correct
     {
         @AfterEach
         void afterEach() throws ServletException, IOException
         {
-            try (val utils = mockStatic(Utils.class, Answers.CALLS_REAL_METHODS))
+            try (val utils = mockStatic(Utils.class, Answers.CALLS_REAL_METHODS);
+                 val magazzinoController = mockStatic(MagazzinoController.class, Answers.CALLS_REAL_METHODS))
             {
-                utils.verify(() -> Utils.dispatchError(any(), any(), any()), never());
+                val prodotto1 = new Prodotto(1, "Farina", "Caputo", 10.00F, 0, null, null, null);
+                val prodotto2 = new Prodotto(5, "Farina", "Caputo", 10.00F, 7.50F, LocalDate.of(2024, 3, 3), LocalDate.of(2024, 3, 5), null);
 
-                new BilancioPeriodoServlet().doPost(request, response);
+                magazzinoController.when(() -> MagazzinoController.getProdottoById(1)).thenReturn(prodotto1);
+                magazzinoController.when(() -> MagazzinoController.getProdottoById(5)).thenReturn(prodotto2);
+
+                val captor = ArgumentCaptor.forClass(String.class);
+                utils.when(() -> Utils.dispatchSuccess(captor.capture(), any(), any())).thenAnswer(Answers.RETURNS_DEFAULTS);
+
+                new ImpostaScontoServlet().doPost(request, response);
+
+                System.out.println(captor.getValue());
             }
         }
+
         @Test
         @DisplayName("5.2.10")
         void tc_5_2_10()
         {
-            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto","2025-01-01"), entry("dataFineSconto","2025-01-02"), entry("sconto","1")));
+            populateRequest(ofEntries(entry("prodotto", "1"), entry("dataInizioSconto", "2025-01-01"), entry("dataFineSconto", "2025-01-02"), entry("sconto", "1")));
         }
     }
 }
